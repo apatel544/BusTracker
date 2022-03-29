f78be147a3af57620736c93f01fef70a736a36dc
apatel544 <ankurpatel544@gmail.com>
2022-03-29 10:09:51 -0400

    uploading files for bus tracker project

---
+++ map.js
@@ -0,0 +1,64 @@
+// This array contains the coordinates for all bus stops between MIT and Harvard
+const busStops = [
+  [-71.093729, 42.359244],
+  [-71.094915, 42.360175],
+  [-71.0958, 42.360698],
+  [-71.099558, 42.362953],
+  [-71.103476, 42.365248],
+  [-71.106067, 42.366806],
+  [-71.108717, 42.368355],
+  [-71.110799, 42.369192],
+  [-71.113095, 42.370218],
+  [-71.115476, 42.372085],
+  [-71.117585, 42.373016],
+  [-71.118625, 42.374863],
+];
+
+// TODO: add your own access token
+
+mapboxgl.accessToken =
+  "pk.eyJ1IjoiYXBhdGVsNTQ0IiwiYSI6ImNsMHRwM3V6eDBmZ3YzY3BiMGxrcGhvZHEifQ.vBH6eNzcAscJsO5nJbEK5w";
+const map = new mapboxgl.Map({
+  container: "map", // container ID
+  style: "mapbox://styles/mapbox/satellite-v9", // style URL
+  center: [-71.104081, 42.365554],
+  zoom: 14,
+});
+
+const layerList = document.getElementById("menu");
+const inputs = layerList.getElementsByTagName("input");
+
+for (const input of inputs) {
+  input.onclick = (layer) => {
+    const layerId = layer.target.id;
+    map.setStyle("mapbox://styles/mapbox/" + layerId);
+  };
+}
+
+// TODO: add a marker to the map at the first coordinates in the array busStops. The marker variable should be named "marker"
+let marker = new mapboxgl.Marker()
+  .setLngLat([-71.092761, 42.357575])
+  .addTo(map);
+
+// counter here represents the index of the current bus stop
+let counter = 0;
+function move() {
+  // TODO: move the marker on the map every 1000ms. Use the function marker.setLngLat() to update the marker coordinates
+  // Use counter to access bus stops in the array busStops
+  // Make sure you call move() after you increment the counter.
+  setTimeout(() => {
+    if (counter >= busStops.length) return;
+    marker.setLngLat(busStops[counter]);
+    counter++;
+    move();
+  }, 1000);
+}
+
+function reset() {
+  window.
+}
+
+// Do not edit code past this point
+if (typeof module !== "undefined") {
+  module.exports = { move };
+}
