b09bafbd09698ef24f6c8857e933c1c38e99ee71
apatel544 <ankurpatel544@gmail.com>
2022-03-29 10:33:38 -0400

    added light, dark and satellite mode to map.

--- map.js
+++ map.js
@@ -20,7 +20,7 @@ mapboxgl.accessToken =
   "pk.eyJ1IjoiYXBhdGVsNTQ0IiwiYSI6ImNsMHRwM3V6eDBmZ3YzY3BiMGxrcGhvZHEifQ.vBH6eNzcAscJsO5nJbEK5w";
 const map = new mapboxgl.Map({
   container: "map", // container ID
-  style: "mapbox://styles/mapbox/satellite-v9", // style URL
+  style: "mapbox://styles/mapbox/navigation-day-v1", // style URL
   center: [-71.104081, 42.365554],
   zoom: 14,
 });
